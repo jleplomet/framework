9b5c46c0d0c7f6fb74a87903c5cdef6aa4c434d9
Jeff <jleplomet@gmail.com>
2015-11-25 12:56:10 -0800

    clean up

--- src/js/components/Index.js
+++ src/js/components/Index.js
@@ -4,11 +4,6 @@ import styles from 'scss/components/index';
 import React, {Component} from 'react';
 
 export default class Index extends Component {
-  componentDidMount() {
-    setTimeout(() => {
-      this.props.navigate('/about?foo=bar')
-    }, 1500);
-  }
 
   render() {
     return (
